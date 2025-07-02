a9f9a4bca93a913bb1cd4257c92a15cb3ad014e9
Arpit tripathi <prabhatshuklaaaaaa@gmail.com>
2025-07-02 15:40:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002728742161))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002728742161))
 
 # Get this value Telegram id
