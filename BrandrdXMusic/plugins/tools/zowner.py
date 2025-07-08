b33209e1c83e7dbe0ff4efae31fb07663035285b
Arpit tripathi <prabhatshuklaaaaaa@gmail.com>
2025-07-08 18:54:48 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -11,13 +11,13 @@ from BrandrdXMusic.utils.database import add_served_chat, get_assistant
 @app.on_message(filters.command("repo"))
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
+        photo=f"https://graph.org/file/2095da87ad6ef87bacae6-e48e749fc322df0865.jpg",
         caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "ƨσʋяcɛ", url=f"https://t.me/Dosti_Ki_Duniya_hindi_Chat_Group"
                     )
                 ]
             ]
@@ -34,7 +34,7 @@ async def clones(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "ƨσʋяcɛ", url=f"https://t.me/Dosti_Ki_Duniya_hindi_Chat_Group"
                     )
                 ]
             ]
